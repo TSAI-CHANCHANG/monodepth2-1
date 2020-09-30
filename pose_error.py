fix the relative pose calculation
The previous version of calculation is wrong.

pose_error.py
@@ -6,8 +6,8 @@ import cv2
 import os
 
 def calRelativePose(poseMatrix1, poseMatrix2):
-    """Calculate the relative pose FROM matrix1 To matrix2
-    # Return Value: relativeMartix
+    """Calculate the relative pose FROM matrix1 To matrix2 using in 7scenes
+    # Return Value: relativeMatrix
     """
     assert (poseMatrix1.shape == (4, 4)), "shape of matrix 1 should be (4,4)"
     assert (poseMatrix2.shape == (4, 4)), "shape of matrix 1 should be (4,4)"
@@ -17,13 +17,13 @@ def calRelativePose(poseMatrix1, poseMatrix2):
     transVec1 = poseMatrix1[0:3, 3]
     transVec2 = poseMatrix2[0:3, 3]
 
-    relativeRotM = rotMatrix1.T @ rotMatrix2
-    relativeTransV = transVec2 - transVec1
-    relativeMatrix = np.zeros((4, 4), dtype=np.float64)
+    poseMatrix2_inv = np.zeros((4, 4), dtype=np.float64)
+    # [R|t]' = [R^T|-R^Tt]
+    poseMatrix2_inv[0:3, 0:3] = rotMatrix2.T
+    poseMatrix2_inv[0:3, 3]  = - np.matmul(rotMatrix2.T, transVec2)
+    poseMatrix2_inv[3, 3]   = 1
 
-    relativeMatrix[0:3, 0:3] = relativeRotM
-    relativeMatrix[0:3, 3] = relativeTransV
-    relativeMatrix[3, 3] = 1
+    relativeMatrix = np.matmul(poseMatrix2_inv, poseMatrix1)
     return relativeMatrix
 
 def calPoseError(gtRelativePose, predictionPose):